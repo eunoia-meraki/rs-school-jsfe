25a19a1909f119c0644633d3e99ce69c569399d0
ivan-kuricyn <kuritsin020994@gmail.com>
2022-01-30 14:46:02 +0300

    fix: fix scrolling

--- art-quiz/webpack.config.js
+++ art-quiz/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: path.resolve(__dirname, '/src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     port: 9000,
